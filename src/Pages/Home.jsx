dc7bf266c5814672a06688f8d5f0c08cc32e23da
ANKITKUMAR1809 <skwedsak143@gmail.com>
2024-05-12 15:36:20 +0530

    changes in links presentation and false data is deleted

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Links } from '../Components/Links/Links'
 import LinkList  from '../Components/assets/my_links.js'
 import './Css/Home.css'
+import banner from '../Components/assets/mybanner.jpg'
 export const Home = () => {
   let my_link = LinkList;
   return (
     <>
+      <img src={banner} alt="" className='banner' />
       <div className="linklist">
         {my_link.map((item, i) => {
           return <Links link={item.link} logo={item.logo} name={item.name} date={item.date} />
